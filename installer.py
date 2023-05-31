88a4d56c3cbe226253769e9bd4372264b30878db
Vahe Ghazaryan <vahe@cloudz.am>
2023-05-31 18:27:23 +0000

    Add install command

--- installer.py
+++ installer.py
@@ -1,22 +1,25 @@
 import json
 from models.binaryInstaller import BinaryInstaller
+from models.packageInstaller import PackageInstaller
 
 
 with open("config.json", "r") as f:
     config = json.load(f)
 
-# print(config)
 install_type = config["install_type"]
 
 settings = config["install_settings"]
 
 params = config["params"]
 
-# print(settings)
-for p in params:
-    print(p["config_key"])
-    print(p["value"])
+if install_type == "binary":
+    service = BinaryInstaller(**settings, config_params=params)
+    try:
+        service.install_service()
 
-
+    except Exception as e:
+        service.remove_service()
+        raise e
+    # service.remove_service()
 
 # apt_installer = BinaryInstaller(**config)
