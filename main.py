95a1ba5272528ed364a794357eeb2bab26c742b4
Vahe Ghazaryan <vahe@cloudz.am>
2023-08-16 10:55:09 +0000

    change main.py logic, file parameter is required

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from json import load, dumps
+from json import load
 from argparse import ArgumentParser
 from src.models.apt_models.apt_package import AptPackage
 from src.models.binary.binary_package import BinaryPackage
@@ -13,37 +13,26 @@ parser.add_argument("-r", "--remove", action="store_true", help="remove service"
 parser.add_argument("-s", "--status", action="store_true", help="check service status")
 parser.add_argument("-ci", "--is-installed", action="store_true",help="check whether service is installed")
 parser.add_argument("-v", "--version", action="store_true", help="check service version")
-parser.add_argument("-f", "--file", help="specify configuration file")
+parser.add_argument("-f", "--file", help="specify configuration file", required=True)
 parser.add_argument("-sr", "--service-exists", action="store_true", help="check if systemd service exists")
 
 args = parser.parse_args()
 
 configfile = args.file
-
 with open(configfile, "r") as f:
     config = load(f)
 
-install_settings = {**config, **config["user_parameters"]}
-del install_settings["user_parameters"]
+if type(config) == dict:
+    install_type = config["install_type"]
+elif type(config) == list:
+    install_type = "docker"
 
-install_type = install_settings["install_type"]
+def load_config(conf):
+    install_settings = {**conf, **conf["user_parameters"]}
+    del install_settings["user_parameters"]
+    return install_settings
 
-if install_type == "apt":
-    service = AptPackage(**install_settings)
-    installer = AptPackageInstaller(service)
-
-elif install_type =="binary":
-    service = BinaryPackage(**install_settings)
-    installer = BinaryInstaller(service)
-
-elif install_type == "docker":
-    installer = DockerInstaller(**install_settings)
-
-else:
-    raise InstallTypeError(f"Unsupported install type: {install_type}")
-
-
-def main():
+def service_action():
     if args.install:
         installer.install_service()
 
@@ -63,5 +52,30 @@ def main():
         print(installer.service_exists())
 
 
-if __name__ == "__main__":
-    main()
+if install_type == "apt":
+    install_settings = load_config(config)
+    service = AptPackage(**install_settings)
+    installer = AptPackageInstaller(service)
+    service_action()
+
+elif install_type =="binary":
+    install_settings = load_config(config)
+    service = BinaryPackage(**install_settings)
+    installer = BinaryInstaller(service)
+    service_action()
+
+elif install_type == "docker":
+    for i in config:
+        install_settings = load_config(i)
+        installer = DockerInstaller(**install_settings)
+        service_action()
+
+else:
+    raise InstallTypeError(f"Unsupported install type: {install_type}")
+
+
+# def main():
+#     pass
+
+# if __name__ == "__main__":
+#     main()
